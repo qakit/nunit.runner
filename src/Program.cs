790561cbf5edc5b0001c4502f0014b1c3ef5ceab
Andrey Egorov <andrey.egorov@grapecity.com>
2018-10-29 09:29:23 +0700

    #1 fix exception. print usage

--- src/Program.cs
+++ src/Program.cs
@@ -10,6 +10,13 @@ namespace nunit.runner
 	{
 		static int Main(string[] args)
 		{
+			if (args.Length == 0)
+			{
+				Console.WriteLine("Usage:");
+				Console.WriteLine("   nunit.runner <testassembly> <options>");
+				return 0;
+			}
+
 			var fileInfo = new FileInfo(args[0]);
 			if(!fileInfo.Exists) throw new ArgumentException($"Invalid parameter for test assembly: {args[0]}. Make sure that first parameter is valid test assembly name and it exists.");
 
